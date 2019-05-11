41f8ecf8642d3d209e6e026790c2c1ce715ea4d0
Huong-DTT <thuhuong160994@gmail.com>
2019-05-11 08:28:39 +0700

    add bai tap tim uoc chung lon nhat

---
+++ src/GreatestCommonDivisor.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class GreatestCommonDivisor {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println("Enter the number a: ");
+        int a = input.nextInt();
+        System.out.println("Enter the number b: ");
+        int b = input.nextInt();
+        a = Math.abs(a);
+        b = Math.abs(b);
+        if (a == 0 || b == 0) {
+            System.out.println("No greatest common factor");
+        }
+        while (a != b) {
+            if (a > b) {
+                a = a - b;
+            } else {
+                b = b - a;
+            }
+        }
+        System.out.println("Greatest common factor:" + a);
+    }
+}
